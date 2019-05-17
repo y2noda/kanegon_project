a4d673482dafde045e58ff79716df048e96e3458
yuhei <yuhei39.apollo@gmail.com>
2019-05-17 14:11:31 +0900

    add: #1 migration & DB

--- server/kanegon/database/migrations/2019_05_17_045541_create_expenses_table.php
+++ server/kanegon/database/migrations/2019_05_17_045541_create_expenses_table.php
@@ -15,6 +15,10 @@ class CreateExpensesTable extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->integer('price');
+            $table->date('purchased_at');
             $table->timestamps();
         });
     }
